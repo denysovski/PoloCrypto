59a7ae5635946673b994d4ea258d111729c60fca
Daniel Svoboda <dankosvobodkaaaa@gmail.com>
2026-02-20 21:39:53 +0100

    Add GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true"
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -6,6 +8,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: "export",
+  trailingSlash: true,
+  basePath: isGithubActions ? "/PoloCrypto" : "",
+  assetPrefix: isGithubActions ? "/PoloCrypto/" : undefined,
 }
 
 export default nextConfig
